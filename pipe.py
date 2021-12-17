db99e5b26dd98798b06e306cb467090ead8a87ae
João Ferreira <joaoprcf@ua.pt>
2021-12-17 00:07:15 +0000

    updated pipe.py variable names and comments, no influence to the solution development

--- pipe.py
+++ pipe.py
@@ -2,29 +2,29 @@
 # one to the stdout of the other.
 # It doesn't perform a lot of checking, so many errors may
 # be caught internally by Python (e.g., if your command line has incorrect
-# syntax) or not caught at all (e.g., if the server or solution hangs).
+# syntax) or not caught at all (e.g., if the server or client hangs).
 #
 # Run this as:
-# python interactive_runner.py <cmd_line_server> -- <cmd_line_solution>
+# python pipe.py <cmd_line_server> -- <cmd_line_client>
 #
 # For example:
-# python interactive_runner.py python testing_tool.py 0 -- ./my_binary
+# python pipe.py python testing_tool.py 0 -- ./my_binary
 #
 # This will run the first test set of a python server called "testing_tool.py"
 # that receives the test set number (starting from 0) via command line parameter
-# with a solution compiled into a binary called "my_binary".
+# with a client compiled into a binary called "my_binary".
 #
 # This code can also be run as python3.
 #
-# This is only intended as a convenient tool to help contestants test solutions
+# This is only intended as a convenient tool to help contestants test clients
 # locally. In particular, it is not identical to the implementation on our
 # server, which is more complex.
 #
 # The standard streams are handled the following way:
-# - server's stdin is connected to the solution's stdout;
-# - server's stdout is connected to the solution's stdin;
-# - stderrs of both server and solution are piped to standard error stream, with
-#   lines prepended by "server: " or "sol: " respectively (note, no
+# - server's stdin is connected to the client's stdout;
+# - server's stdout is connected to the client's stdin;
+# - stderrs of both server and client are piped to standard error stream, with
+#   lines prepended by "server: " or "client: " respectively (note, no
 #   synchronization is done so it's possible for the messages from both programs
 #   to overlap with each other).
 
@@ -83,15 +83,15 @@ sep_index = sys.argv.index("--")
 server_args = sys.argv[1:sep_index]
 sol_args = sys.argv[sep_index + 1:]
 
-t_sol = SubprocessThread(sol_args, stderr_prefix="client debug: ")
+t_client = SubprocessThread(sol_args, stderr_prefix="client debug: ")
 t_server = SubprocessThread(
     server_args,
-    stdin_pipe=t_sol.p.stdout,
-    stdout_pipe=t_sol.p.stdin,
+    stdin_pipe=t_client.p.stdout,
+    stdout_pipe=t_client.p.stdin,
     stderr_prefix="server debug: ")
-t_sol.start()
+t_client.start()
 t_server.start()
-t_sol.join()
+t_client.join()
 t_server.join()
 
 # Print an empty line to handle the case when stderr doesn't print EOL.
@@ -100,6 +100,6 @@ print("server return code:", t_server.return_code)
 if t_server.error_message:
   print("server error message:", t_server.error_message)
 
-print("Solution return code:", t_sol.return_code)
+print("Client return code:", t_client.return_code)
 if t_server.error_message:
-  print("Solution error message:", t_sol.error_message)
+  print("Client error message:", t_client.error_message)
